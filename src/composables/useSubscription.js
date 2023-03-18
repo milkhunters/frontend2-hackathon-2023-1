d0f38aef8709d643064ec2d8ab0d5f35601500bb
iRobbKing <s2068867@iivt.donstu.ru>
2023-03-18 19:00:39 +0300

    m comoposition

--- src/composables/useSubscription.js
+++ src/composables/useSubscription.js
@@ -4,6 +4,7 @@ const useSubscription = (subscribe, unsubscribe, init = null) => {
   const value = ref(init);
 
   const changeValue = (newValue) => (value.value = newValue);
+
   onMounted(() => subscribe(changeValue));
   onUnmounted(() => unsubscribe(changeValue));
 
