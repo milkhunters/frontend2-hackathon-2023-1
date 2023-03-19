dc22eaccb8380270c56aab1e52c93b1318288cc4
iRobbKing <s2068867@iivt.donstu.ru>
2023-03-19 12:19:49 +0300

    f

--- src/lib/api/file/file.js
+++ src/lib/api/file/file.js
@@ -6,7 +6,7 @@ const API_FILE_DOWNLOAD_URL = "file/download"
 export const getFileUrl = (id) => `${API_FILE_DOWNLOAD_URL}/${id}`;
 
 export const saveFile = (file) => {
-  if (file.size < 20 * 1024 * 1024) return;
+  if (file.size > 20 * 1024 * 1024) return;
   return makeWriteRequest(API_FILE_SAVE_URL, encodeFile(file));
 };
 
